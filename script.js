7bc1774da80aa524c1a24ddc5e60e5ce3ab062e1
Timothy Bingtan <tim@Timothys-MacBook-Air.local>
2024-11-09 23:03:46 -0800

    Email validation to input fields

--- script.js
+++ script.js
@@ -2,16 +2,28 @@ document.getElementById("userForm").addEventListener("submit", function(event){
     event.preventDefault();
 
     const email = document.getElementById('email').value;
-    document.getElementById('confirmationMessage').innerHTML = `A confirmation email has been sent to ${email}. Please open it and click the button inside to confirm your subscription.`;
+    var validEmail = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+    if (!email.match(validEmail)){
+        console.log('got here');
+        document.getElementById('email').color = "#FF6155";
+        document.getElementById('email').borderColor = "#FF6155";
+        document.getElementById('email').backgroundColor = "hsla(0, 100%, 50%, 0.15)";
+        document.getElementById('email').outline = 0;
+        document.getElementById('errorMessage').visibility = "visible";
+        console.log('got here as well');
+    } else { 
+        document.getElementById('confirmationMessage').innerHTML = `A confirmation email has been sent to ${email}. Please open it and click the button inside to confirm your subscription.`;
+        
+        document.getElementById('email').value = "";
     
-    document.getElementById('email').value = "";
-
-    const elements = document.querySelectorAll(".initial");
-    elements.forEach(element => {
-        element.style.display = "none";
-    });
-
-    document.getElementById("successMessage").style.display = "block";
+        const elements = document.querySelectorAll(".initial");
+        elements.forEach(element => {
+            element.style.display = "none";
+        });
+    
+        document.getElementById("successMessage").style.display = "block";
+    }
 });
 
 
